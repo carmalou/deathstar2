8a31a7c19056d6a49058bd3527ed18860a1a79b9
carmalou <carmenbourlonlong@gmail.com>
2018-05-13 15:41:41 -0500

    this layout suckssss

--- src/components/individual-troopers.component.js
+++ src/components/individual-troopers.component.js
@@ -18,9 +18,9 @@ export default class IndividualTrooper extends Component {
 
     render() {
         return (
-            <li className="list-group-item" key={ this.props.trooper.name }>
-                <span>{ this.props.trooper.name }</span>
-                <span><button onClick={ this.ToggleDetails }>Show Details</button></span>
+            <li className="list-group-item col-md-12" key={ this.props.trooper.name }>
+                <span className="col-md-6">{ this.props.trooper.name }</span>
+                <span className="col-md-6"><button onClick={ this.ToggleDetails } className="btn btn-info">Show Details</button></span>
                 { this.state.toggle ? <RoomAccess trooper={ this.props.trooper } /> : '' }
             </li>
         )
