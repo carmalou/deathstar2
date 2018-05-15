d16e1001d8e9f2e7deab09b3f79e1b4675f1419e
carmalou <carmenbourlonlong@gmail.com>
2018-05-15 10:24:36 -0500

    :sob:

--- src/actions/indexeddb/editExistingTrooper.js
+++ src/actions/indexeddb/editExistingTrooper.js
@@ -1,21 +1,16 @@
-export function editTrooper(troopers) {
-    if(!troopers || troopers.length <= 0) {
+export function editTrooper(trooper) {
+    if(!trooper) {
         return;
     }
-
-    console.log(troopers);
+    
+    console.log(trooper);
     var db = window.indexedDB.open('deathstar2');
 
     db.onsuccess = function(event) {
         var db = event.target.result;
         var transaction = db.transaction("stormtroopers", "readwrite").objectStore("stormtroopers");
-        for(var i = 0; i < troopers.length; i++) {
-            var tmpTrooper = transaction.get(troopers[i].name);
-            tmpTrooper.onsuccess = function(event) {
-                console.log(event.target.result);
-            }
-        }
+        // var trooper = transaction.get(trooper.name);
 
-        console.log(troopers);
+        console.log(trooper);
     }
 }
--- src/components/room-access.component.js
+++ src/components/room-access.component.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { editTrooper } from '../actions/indexeddb/editExistingTrooper';
 
 export default class RoomAccess extends Component {
 
     constructor() {
         super();
+        this.EditTrooperHandler = this.EditTrooperHandler.bind(this);
+    }
+
+    EditTrooperHandler(trooper) {
+        editTrooper(trooper);
     }
 
     render() {
         return(
+            <div>
             <ul className="list-group">
                 <li className="list-group-item">Trash compactor <input type="checkbox" name="trashCompactorAccess" checked={ this.props.trooper.trashCompactorAccess } data-index={ this.props.index } onChange={ this.props.HandleCheckbox } /></li>
                 <li className="list-group-item">Mess Hall <input type="checkbox" name="messHallAccess" checked={ this.props.trooper.messHallAccess } data-index={ this.props.index } onChange={ this.props.HandleCheckbox } /></li>
                 <li className="list-group-item">Landing Strip <input type="checkbox" name="landingStripAccess" checked={ this.props.trooper.landingStripAccess } data-index={ this.props.index } onChange={ this.props.HandleCheckbox } /></li>
                 <li className="list-group-item">Armory <input type="checkbox" name="armoryAccess" checked={ this.props.trooper.armoryAccess } data-index={ this.props.index } onChange={ this.props.HandleCheckbox } /></li>
             </ul>
+            <span className="list-group-item"><button className="btn btn-primary" onClick={ this.EditTrooperHandler(this.props.trooper) }>Save</button></span>
+            </div>
         )
     }
 }
